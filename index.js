504ec5dd0be6ba426cd766ba61974832b171e16a
navaneeth k <navaneethsaj@gmail.com>
2020-07-21 00:23:04 +0530

    proc file

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ var io = require('socket.io')(http);
 var cors = require('cors')
 var bodyParser = require('body-parser')
 var corsOptions = {
-    origin: ['http://localhost:8100', ],
+    origin: [
+        'capacitor://localhost',
+        'ionic://localhost',
+        'http://localhost',
+        'http://localhost:8080',
+        'http://localhost:8100',
+        'http://localhost:8101',
+        'http://localhost:3200',
+        // 'https://bingo5x5.herokuapp.com',
+        // 'https://bingo5x5-2.herokuapp.com'
+    ],
+
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors(corsOptions))
