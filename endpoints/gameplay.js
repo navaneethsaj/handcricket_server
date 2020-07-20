908ed423edefcfeaa07d002401d74710d3b9e673
navaneeth k <navaneethsaj@gmail.com>
2020-07-21 00:18:14 +0530

    done

--- endpoints/gameplay.js
+++ endpoints/gameplay.js
@@ -1,6 +1,7 @@
 const AsyncLock = require('async-lock');
 const randomstring = require('randomstring')
 let counterLock = new AsyncLock();
+let counterOnlineLock = new AsyncLock();
 let waitingListLock = new AsyncLock();
 let io;
 let playerSockets = {
@@ -13,7 +14,13 @@ let liveGames = {
     waiting: [],
     playingSockets: {},
 }
+let onlineUsers = { count : 0, socketsidtouser: {}, useridtosockets: {}
+}
+let liveInvites = {
+
+}
 function checkAndAddToWaitingList(socket, data) {
+    console.log('s', socket.id)
     // console.log('waiting', liveGames.waiting.length)
     if (liveGames.waiting.includes(socket)){
         console.log('already in WL')
@@ -23,13 +30,14 @@ function checkAndAddToWaitingList(socket, data) {
     waitingListLock.acquire("key", (done) => {
         if (liveGames.waiting.length > 0){
             let waitingMan = liveGames.waiting.shift();
-            let waitingManName = playerSockets[waitingMan].name;
-            console.log('1', waitingManName)
+            let waitingManName = playerSockets.sockets[waitingMan.id].name;
+            // console.log('1', waitingManName)
             done(false, {socket: waitingMan, waitingManName: waitingManName})
         }else {
-            console.log('2', data.name, )
+            // console.log('2', data.name, )
             liveGames.waiting.push(socket);
-            playerSockets[socket] = {name: data.name};
+            playerSockets.sockets[socket.id].name =  data.name;
+            // console.log('ss', playerSockets)
             done(true, {socket, waitingManName: null})
         }
     }, (waiting, otherData) =>{
@@ -51,8 +59,8 @@ function checkAndAddToWaitingList(socket, data) {
             }
             waitingMan.emit('foundgame', {gameid, playerid: 0, bet: liveGames.games[gameid].bet === 0, opponentname: data.name})
             socket.emit('foundgame', {gameid, playerid:1, turn: 0, bet: liveGames.games[gameid].bet === 1, opponentname: waitingName})
-            liveGames.playingSockets[socket] = gameid;
-            liveGames.playingSockets[socket] = gameid;
+            liveGames.playingSockets[socket.id] = gameid;
+            liveGames.playingSockets[waitingMan.id] = gameid;
             // console.log(gameid)
         }
     })
@@ -97,13 +105,14 @@ function removeFromLiveGame(socket) {
 }
 
 function removeFromPlayerSockets(socket) {
-    delete playerSockets[socket]
+    delete playerSockets.sockets[socket.id]
 }
 
 function gameOverClean() {
 
 }
 function initListeners(socket){
+    playerSockets.sockets[socket.id] = {socket}
     socket.on('init', (data) => {
         console.log(data)
     })
@@ -127,15 +136,21 @@ function initListeners(socket){
         let move = data.move;
         let game = liveGames.games[gameId]
         game[playerId].moves.push(move);
+        console.log(gameId)
+        console.log(game)
+        // return
         if (game.batting === playerId){
             game[playerId].score += move;
         }
         console.log(playerId, game[playerId])
         if (game[playerId].moves.length === game[(playerId + 1) % 2].moves.length){
-            console.log('move result')
+            console.log('a')
+            // console.log('move result', data)
             let moveresult = {0: game[0], 1: game[1]};
             if (game[playerId].moves[game[playerId].moves.length - 1] ===
                 game[(playerId + 1) % 2].moves[game[(playerId + 1) % 2].moves.length - 1]){
+                console.log('c')
+
                 // wicket
                 game.wicket++;
                 if (game.wicket >= 2){
@@ -169,6 +184,7 @@ function initListeners(socket){
                 }
             }
             else {
+                console.log('d')
                 // scored
                 if (game.wicket >= 1
                     && game[game.batting].score > game[(game.batting + 1 ) % 2].score){
@@ -186,20 +202,23 @@ function initListeners(socket){
                     })
                 }
                 else {
+                    console.log('e', game.sockets.length)
                     game.sockets.forEach(sock => {
+                        console.log('sock', sock.id)
                         sock.emit('moveresult', moveresult)
                     })
                 }
 
             }
         } else {
+            console.log('a')
             game.sockets.forEach(sock => {
                 console.log('opp chosed')
                 sock.emit('opponentchosed', {playerid: playerId})
             })
 
         }
-
+        console.log('end')
     })
     socket.on('choice', (data) => {
         let gameid = data.gameid;
@@ -224,13 +243,122 @@ function initListeners(socket){
         removeFromPlayerSockets(socket)
     });
 }
+function initListenersForOnline(socket){
+    console.log(onlineUsers)
+    onlineUsers.socketsidtouser[socket.id] = {
+    }
+    console.log(onlineUsers)
+    socket.on('disconnect', () => {
+        console.log('online user disconnected');
+        incrementDecrementOnlineCount(false)
+        delete onlineUsers.socketsidtouser[socket.id]
+    });
+    socket.on('customgame', (data) => {
+        console.log('custom game');
+        let playername = data.playername;
+        let oppuserid = data.uniqueidopp;
+        let playerid = data.uniqueidplayer;
+        // playerSockets.sockets[socket.id].name =  data.name;
+        // data.finalgamesockets[0].emit('test', "testing this")
+        // console.log('sa', data.finalgamesocketsid[0],)
+        // console.log('s', playerSockets,)
+        playerSockets.sockets[data.finalgamesocketsid].name =  data.name;
+        let tsocket = playerSockets.sockets[data.finalgamesocketsid].socket
+        // console.log('tsock', tsocket)
+        // tsocket.emit('test', 'test')
+        let oppuser = onlineUsers.useridtosockets[oppuserid];
+        let inviteid = randomstring.generate();
+        liveInvites[inviteid] = {
+            gameSockets: tsocket,
+            playername
+        }
+        oppuser.socket.emit('invite', {playername, playerid, inviteid})
+    });
+    socket.on('inviteresponse', (data) => {
+        let action = data.action;
+        let noftifyplayerid = data.notifyplayerid;
+        let oppSock = onlineUsers.useridtosockets[noftifyplayerid].socket;
+        if (action === 'accept'){
+            oppSock.emit('inviteresponded', data)
+        }else if (action === 'reject'){
+            oppSock.emit('inviteresponded', data)
+        }
+    })
+    socket.on('joinedinvite', (data) => {
+        let inviteid = data.inviteid;
+        let mygamesocket = playerSockets.sockets[data.mygamesocketid].socket
+        let playerSocks1 = liveInvites[inviteid].gameSockets
+        let playerSocks2 = mygamesocket;
+        // player 1 is first player
+        let player1Name = liveInvites[inviteid].playername;
+        let player2Name = data.playername;
+        console.log('ps', player1Name, player2Name)
+        const gameid = randomstring.generate();
+        liveGames.games[gameid] = {
+            lastPing: new Date(),
+            turn: 0,
+            sockets: [playerSocks1, playerSocks2],
+            0: {moves: [], score: 0, name: player1Name},
+            1: {moves: [], score: 0, name: player2Name},
+            bet: Math.floor((Math.random() * 2)),
+            batting: null,
+            wicket : 0,
+        }
+        // playerSocks1.emit('test', 'test')
+        // playerSocks2.emit('test', 'test')
+        playerSocks1.emit('foundgame', {gameid, playerid: 0, bet: liveGames.games[gameid].bet === 0, opponentname: player2Name})
+        playerSocks2.emit('foundgame', {gameid, playerid:1, turn: 0, bet: liveGames.games[gameid].bet === 1, opponentname: player1Name})
+        liveGames.playingSockets[playerSocks1.id] = gameid;
+        liveGames.playingSockets[playerSocks2.id] = gameid;
+    })
+    socket.on('playerdata', (data) => {
+        let playername = data.name;
+        let uniqueid = data.uniqueid;
+        let score = data.score;
+        console.log('playername', playername, uniqueid);
+        onlineUsers.socketsidtouser[socket.id].name = playername;
+        onlineUsers.socketsidtouser[socket.id].uniqueid = uniqueid;
+        onlineUsers.socketsidtouser[socket.id].score = score;
+        onlineUsers.useridtosockets[uniqueid] = {socket, playername, score};
+        // console.log(onlineUsers)
+    });
+    socket.on('getonlineplayers', () => {
+        let users = []
+        for (let sockid in onlineUsers.socketsidtouser){
+            let user = onlineUsers.socketsidtouser[sockid]
+            users.push(user)
+        }
+        // console.log(users);
+        socket.emit('onlineplayers', users);
+    });
+}
+function incrementDecrementOnlineCount(increment){
+    counterOnlineLock.acquire("key", function(done) {
+        // async work
+        if (increment){
+            onlineUsers.count++;
+        }else {
+            onlineUsers.count--;
+        }
+        done();
+    }, function() {
+        // lock released
+        console.log('online user Count changed ', onlineUsers.count)
+    });
+}
+
 module.exports = function (io_) {
     console.log('initialised socket io')
     io = io_;
-    io.on('connection', (socket) => {
-        console.log('a user connected');
+    io.of('/multiplayer').on('connection', (socket) => {
+        console.log('a user connectez', socket.id);
         initListeners(socket);
         incrementDecrementPlayerCount(true);
+    });
+    io.of('/online').on('connection', (socket) => {
+        console.log('a user connected');
+        initListenersForOnline(socket);
+        incrementDecrementOnlineCount(true);
     });
 }
 
