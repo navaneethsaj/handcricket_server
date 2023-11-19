52f14d4532d3d66a99e4ecc92ffeae75f07eb45c
Nk <navaneethsaj@gmail.com>
2023-11-19 18:20:37 +0530

    Upgrade cors to https

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
 const PORT = process.env.PORT || 3000;
-var express = require('express')
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var cors = require('cors')
-var bodyParser = require('body-parser')
+var express = require("express");
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+io.origins("*:*");
+var cors = require("cors");
+var bodyParser = require("body-parser");
 var corsOptions = {
-    origin: [
-        'capacitor://localhost',
-        'ionic://localhost',
-        'http://localhost',
-        'http://localhost:8080',
-        'http://localhost:8100',
-        'http://localhost:8101',
-        'http://localhost:3000',
-        // 'https://bingo5x5.herokuapp.com',
-        // 'https://bingo5x5-2.herokuapp.com'
-    ],
+  origin: [
+    "capacitor://localhost",
+    "ionic://localhost",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8100",
+    "http://localhost:8101",
+    "http://localhost:3000",
 
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-app.use(cors(corsOptions))
-app.use(bodyParser.json())
-app.use(express.static('public'))
-var socket = require('./endpoints/gameplay')
-socket(io)
-var scoreboard = require('./endpoints/scoreboard')
-var users = require('./endpoints/users')
+    "https://localhost",
+    "https://localhost:8080",
+    "https://localhost:8100",
+    "https://localhost:8101",
+    "https://localhost:3000",
+  ],
 
-const startDate = new Date()
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+var socket = require("./endpoints/gameplay");
+socket(io);
+var scoreboard = require("./endpoints/scoreboard");
+var users = require("./endpoints/users");
 
-app.get('/', (req, res) => {
-    res.send({
-        status: "ok",
-        runningSince: startDate
-    })
+const startDate = new Date();
+
+app.get("/", (req, res) => {
+  res.send({
+    status: "ok",
+    runningSince: startDate,
+  });
 });
 
-app.use('/scoreboard', scoreboard);
-app.use('/users', users);
-
+app.use("/scoreboard", scoreboard);
+app.use("/users", users);
 
 http.listen(PORT, () => {
-    console.log('listening on http://localhost:3000');
+  console.log("listening on http://localhost:3000");
 });
